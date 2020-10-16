e5f9a0fac96270eae7f5da214b8e1e4e7370e8cc
valbornoz <victor.lalbornoz@hotmail.com>
2020-10-15 22:16:41 -0300

    version 1.8.8 (cambio de dominio)

--- src/config/url.servicios.ts
+++ src/config/url.servicios.ts
@@ -1,2 +1,2 @@
-export const URL_SERVICIOS = "http://pedidostopauto.com/REST/index.php";
+export const URL_SERVICIOS = "http://pedidostopauto.online/REST/index.php";
 // export const URL_SERVICIOS = "http://localhost/REST/index.php";
